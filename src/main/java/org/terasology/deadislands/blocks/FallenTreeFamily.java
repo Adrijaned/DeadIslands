2c2a56c0c128ca189f668666b3787dbf87588c31
Adrijaned <adrijaned@gmail.com>
2018-01-08 07:09:58 +0100

    Works perfectly now

--- src/main/java/org/terasology/deadislands/blocks/FallenTreeFamily.java
+++ src/main/java/org/terasology/deadislands/blocks/FallenTreeFamily.java
@@ -58,7 +58,7 @@ public class FallenTreeFamily extends MultiConnectFamily {
 
     @Override
     public byte getConnectionSides() {
-        return SideBitFlag.getSides(Side.LEFT, Side.RIGHT);
+        return SideBitFlag.getSides(Side.LEFT, Side.RIGHT, Side.FRONT, Side.BACK);
     }
 
     @Override
